ab68fdd44b069401ae004f8eedb8c6972c8c6df0
Akeel <agilowen@gmail.com>
2014-03-10 19:57:54 -0400

    created Mailbox

---
+++ src/Mailbox.java
@@ -0,0 +1,73 @@
+/**
+ * A mailbox contains messages that can be listed, kept or discarded
+ * @author Akeel
+ *
+ */
+public class Mailbox {
+	/**
+	 * Creates Mailbox object.
+	 * @param aPasscode passcode number
+	 * @param aGreeting greeting string
+	 */
+	public Mailbox(String aPasscode, String aGreeting) {
+		passcode = aPasscode;
+		greeting = aGreeting;
+		newMessages = new MessageQueue();
+		keptMessages = new MessageQueue();
+	}
+	
+	/**
+	 * Check if the passcode is correct.
+	 * @param aPasscode a passcode to check
+	 * @return true if the supplied passcode matches the mailbox passcode
+	 */
+	public boolean checkPasscode(String aPasscode) {
+		return aPasscode.equals(passcode);
+	}
+	
+	public void addMessage(Message aMessage) {
+		newMessages.add(aMessage);
+	}
+	
+	public Message getCurrentMessage() {
+		if (newMessages.size() > 0)
+			return newMessages.peek();
+		else if (keptMessages.size() > 0)
+			return keptMessages.peek();
+		else
+			return null;
+	}
+	
+	public Message removeCurrentMessage() {
+		if (newMessages.size() > 0)
+			return newMessages.remove();
+		else if (keptMessages.size() > 0)
+			return keptMessages.remove();
+		else
+			return null;
+	}
+	
+	public void saveCurrentMessage() {
+		Message m = removeCurrentMessage();
+		
+		if (m != null)
+			keptMessages.add(m);
+	}
+	
+	public void setGreeting(String newGreeting) {
+		greeting = newGreeting;
+	}
+	
+	public void setPasscode(String newPasscode) {
+		passcode = newPasscode;
+	}
+	
+	public String getGreeting() {
+		return greeting;
+	}
+	
+	private MessageQueue newMessages;
+	private MessageQueue keptMessages;
+	private String passcode;
+	private String greeting;
+}
